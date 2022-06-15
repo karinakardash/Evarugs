aba8c057fff62ff5224b29cf66f62a1454b62d09
KarinaKushnir <karina.kushnir97@gmail.com>
2022-06-15 21:18:02 +0300

    add project evarugs

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    initialSlide:1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    slidesPerView: 3,
+    spaceBetween: 70,
+    grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+  });
